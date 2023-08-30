c6d4b61af7206e818ffed789469e80dcab6bb53b
SHjNEe <baotrung.jpvn@gmail.com>
2023-08-30 23:25:26 +0700

    Add

--- ScrapingData/main.py
+++ ScrapingData/main.py
@@ -1,9 +1,82 @@
-import requests
-from bs4 import BeautifulSoup
-file_path = "index.html"
-res = requests.get('https://www.eggtrading.com/')
-soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.body)
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.selector import Selector
+import pprint
+import csv
+
+class HackerNewsSpider(scrapy.Spider):
+    name = 'hacker_news'
+    start_urls = ['https://news.ycombinator.com/news', 'https://news.ycombinator.com/news?p=2']
+
+    def parse(self, response):
+        links = response.css('.titleline > a')
+        subtext = response.css('.subtext')
+
+        hn = []
+        for idx, item in enumerate(links):
+            title = item.css('::text').get()
+            href = item.css('::attr(href)').get()
+            vote = subtext[idx].css('.score')
+            if vote:
+                points = int(vote.css('::text').get().replace(' points', ''))
+                if points > 99:
+                    hn.append({'title': title, 'link': href, 'votes': points})
+        pprint.pprint(hn)
+        # Write the data to a CSV file
+        with open('hacker_news.csv', 'w', newline='') as csvfile:
+            fieldnames = ['Title', 'Link', 'Votes']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(hn)
+
+        return self.sort_stories_by_votes(hn)
+
+    def sort_stories_by_votes(self, hnlist):
+        return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+process = CrawlerProcess()
+process.crawl(HackerNewsSpider)
+process.start()
+
+
+# import requests
+# from bs4 import BeautifulSoup
+# import pprint
+
+# res = requests.get('https://news.ycombinator.com/news')
+# res2 = requests.get('https://news.ycombinator.com/news?p=2')
+# soup = BeautifulSoup(res.text, 'html.parser')
+# soup2 = BeautifulSoup(res2.text, 'html.parser')
+
+# links = soup.select('.titleline > a') #heads up! .storylink changed to .titleline
+# subtext = soup.select('.subtext')
+# links2 = soup2.select('.titleline > a') #heads up! .storylink changed to .titleline
+# subtext2 = soup2.select('.subtext')
+
+# mega_links = links + links2
+# mega_subtext = subtext + subtext2
+
+# def sort_stories_by_votes(hnlist):
+#   return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+
+# def create_custom_hn(links, subtext):
+#   hn = []
+#   for idx, item in enumerate(links):
+#     title = item.getText()
+#     href = item.get('href', None)
+#     vote = subtext[idx].select('.score')
+#     if len(vote):
+#       points = int(vote[0].getText().replace(' points', ''))
+#       if points > 99:
+#         hn.append({'title': title, 'link': href, 'votes': points})
+#   return sort_stories_by_votes(hn)
+ 
+# pprint.pprint(create_custom_hn(mega_links, mega_subtext))
+
+
+
+
 # with open(file_path, "w") as file:
 #     file.write(res.text)
 
